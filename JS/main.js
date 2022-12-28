08ce5bccfdbf1503d347ba1f8a00254ef56d011e
LizavetBa <vetanikis@gmail.com>
2022-12-28 17:26:47 +0100

    created home,about,projects and contacts. not finished yet

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,16 @@
-const buttonEl = document.getElementById('button');
-buttonEl.addEventListener('click',() => {
-    buttonEl.classList.toggle('_action');
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
+let activeLink = 0;
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    })
 })
